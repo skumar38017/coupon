33130d3702376b4533467038a059f8f9aa7fc48e
s_kumar <152770548+skumar38017@users.noreply.github.com>
2024-12-09 00:17:29 +0530

    Update coupon (#1)
    * add config.js
    
    * update congig in script.js
    
    ---------
    
    Co-authored-by: skumar <skumar38017@outlook.com>

--- config.js
+++ config.js
@@ -1,9 +1,21 @@
+// This is the config.js file for the coupon website
+// It contains the configuration values for the offers
+// config.js
+
 const config = {
-    cashback: "Flat 16.8%",
-    currency: "₹",
-    price: "399", // Price without the currency symbol
-    startDate: "11/22/2024",
-    endDate: "12/31/2024",
-    promoCode: "SAVE20",
-    offerDetails: "Flat 16.8% cashback and additional discounts on this product."
+    currency: "₹",  // Currency symbol
+    offers: [
+        {
+            imageUrl: "https://d3pzq99hz695o4.cloudfront.net/temporary/image/20241113105554-766110-190x260.png",
+            storeLogo: "https://d3pzq99hz695o4.cloudfront.net/sitespecific/in/stores/web/a5191baa4fad6125a33902117e5e4847/sanfe-coupons-logo-large.jpg?455795",
+            title: "Retinol Body ",
+            cashback: "Flat 16.8%",
+            price: "399", // Price without currency symbol
+            startDate: "11/22/2024",
+            endDate: "12/31/2024",
+            promoCode: "SAVE20",
+            details: "Flat 16.8% cashback and additional discounts on this product."
+        },
+        // Add more offers as needed
+    ]
 };
